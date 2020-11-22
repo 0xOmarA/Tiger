91e4d8ca1a9fe0ccda97470c8502f24e7646fabf
0xOmarA <OmarAbdulla7@hotmail.com>
2020-11-22 17:30:18 +0300

    Added more to readme.md

--- Tiger/Program.cs
+++ Tiger/Program.cs
@@ -13,7 +13,44 @@ namespace Tiger
         {
             string destiny_path = @"C:\Program Files (x86)\Steam\steamapps\common\Destiny 2\packages";
             string extraction_path = @"C:\D2PkgExtractionPath";
-            Extractor extractor = new Extractor(destiny_path, LoggerLevels.Disabled);
+            Extractor extractor = new Extractor(destiny_path, LoggerLevels.HighVerbouse);
+        }
+    }
+}
+
+namespace Tiger
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            string packages_path = @"C:\Program Files (x86)\Steam\steamapps\common\Destiny 2\packages";
+            string output_path = @"C:\DestinyExtractionPath";
+            Extractor extractor = new Extractor(packages_path, LoggerLevels.HighVerbouse);
+
+            foreach (Package package in extractor.master_packages_stream())
+            {
+                for(int entry_index = 0; entry_index<package.entry_table().Count; entry_index++)
+                {
+                    Parsers.ParsedFile file;
+
+                    Formats.Entry entry = package.entry_table()[entry_index];
+                    switch(entry.entry_a)
+                    {
+                        case (uint)Blocks.Type.StringBank:
+                            file = new Parsers.StringBankParser(package, entry_index, extractor).Parse();
+                            file.WriteToFile(output_path);
+                            break;
+
+                        case (uint)Blocks.Type.StringReferenceIndexer:
+                            file = new Parsers.StringReferenceIndexerParser(package, entry_index, extractor).Parse();
+                            file.WriteToFile(output_path);
+                            break;
+                    
+                        //Other cases for other block types go here
+                    }
+                }
+            }
         }
     }
 }
