412ce58c71ea5056be3b4ab868d6291a65f1d6a0
0xOmarA <OmarAbdulla7@hotmail.com>
2020-11-24 15:31:41 +0300

    Removed the TigerViewer because its very buggy

--- TigerViewer/Controllers/TigerIntegration.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-namespace TigerViewer.Controllers
-{
-    public static class TigerIntegration
-    {
-        public static Tiger.Extractor extractor = new Tiger.Extractor("/Volumes/WinToUSB/Program Files (x86)/Steam/steamapps/common/Destiny 2/packages", true);
-    }
-}
